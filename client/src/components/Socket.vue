a72a617ee149f2ae29fe398e3ab960acf91fd890
kristapsv <kristapsvitols91@gmail.com>
2019-06-26 17:14:11 +0300

    remove unnecessary uri

--- client/src/components/Socket.vue
+++ client/src/components/Socket.vue
@@ -25,7 +25,7 @@
                 user: '',
                 message: '',
                 messages: [],
-                socket: io('localhost:8080'),
+                socket: io(),
             }
         },
         async mounted() {
